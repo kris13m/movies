845354e147f9f0a0cd0fdf6f849077cef533f4cb
Kristian Mortensen <kristianbmortensen@gmail.com>
2025-05-17 19:52:57 +0200

    update server.js
    small update

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,8 @@ const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Core middleware
 app.use(express.json());
-
-
 app.use(helmet());
-
 app.use(cookieParser());
 
 const corsPolicy = {
@@ -22,23 +18,17 @@ const corsPolicy = {
 };
 
 app.options('*', cors(corsPolicy));
-
 app.use(cors(corsPolicy));
 
-
-
-// Health check
-app.get('/', (req, res) => {
-  res.send('API is running');
-});
-
-
 const initializeApp = require('./config/initialize');
-const { cookie } = require('express-validator');
 
-initializeApp(app).then(() => {
-  app.listen(3000, () => console.log('API is running on http://localhost:'+PORT || 3000));
-}).catch((err) => {
-  console.error('Initialization error:', err);
-  process.exit(1);
-});
+initializeApp(app)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`API is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Initialization error:', err);
+    process.exit(1);
+  });
